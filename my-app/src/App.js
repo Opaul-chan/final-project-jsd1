e4371cadb3536064bcfd00133f60e4ca15bd9cb0
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-03-21 18:47:05 +0700

    "update add activity botton"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import ActivityIcon from "./Components/ActivityIcon/ActivityIcon.jsx";
 //import "./App.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import ActivityForm from "./Components/ActivityForm/ActivityForm.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ActivityIcon />
       <ActivityForm />
+      <Footer />
     </div>
   );
 }
--- my-app/src/Components/ActivityForm/ActivityForm.jsx
+++ my-app/src/Components/ActivityForm/ActivityForm.jsx
@@ -1,32 +1,46 @@
 import React from "react";
-import './ActivityForm.css'
+import "./ActivityForm.css";
 
 const ActivityForm = () => {
-    return(
-        <section className="form-part">
-        <div className="container-fluid">
-          <div className="your-exercise"><h2>Your Exercise Journal</h2></div>
-          <div className="row">
-            {/* <!--part Activity Name input type text--> */}
-           <div className="col-12">
-             <h3>Activity Name</h3>
-             <div className="input-group mb-3">
+  return (
+    <section className="form-part">
+      <div className="container-fluid">
+        <div className="your-exercise">
+          <h2>Your Exercise Journal</h2>
+        </div>
+        <div className="row">
+          {/* <!--part Activity Name input type text--> */}
+          <div className="col-12">
+            <h3>Activity Name</h3>
+            <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1"></span>
-              <input type="text" className="form-control" placeholder="running with my dog." aria-label="Username" aria-describedby="basic-addon1" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="running with my dog."
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
             </div>
           </div>
           {/* <!--part Activity Date input type text--> */}
-           <div className="col-12">
-             <h3>Activity Date</h3>
-             <div className="input-group mb-3">
+          <div className="col-12">
+            <h3>Activity Date</h3>
+            <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1"></span>
-              <input type="text" className="form-control" placeholder="dd/mm/yy" aria-label="Username" aria-describedby="basic-addon1" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="dd/mm/yy"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
             </div>
           </div>
-           {/* <!--part Activity Type input type select--> */}
-           <div className="col-12">
-             <h3>Activity Type</h3>
-             <div className="input-group mb-3">
+          {/* <!--part Activity Type input type select--> */}
+          <div className="col-12">
+            <h3>Activity Type</h3>
+            <div className="input-group mb-3">
               <select className="form-select" id="inputGroupSelect02">
                 <option selected>running</option>
                 <option value="1">running</option>
@@ -42,29 +56,52 @@ const ActivityForm = () => {
                 <option value="2">golf</option>
                 <option value="3">other</option>
               </select>
-              <label className="input-group-text" for="inputGroupSelect02">Options</label>
-            </div>
-            </div>
-            {/* <!--part Activity Duration input type number--> */}
-           <div className="col-12">
-             <h3>Activity Duration</h3>
-             <div className="input-group mb-3">
-              <input type="number" className="form-control" placeholder="60" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-              <span className="input-group-text" id="basic-addon2">mins</span>
+              <label className="input-group-text" for="inputGroupSelect02">
+                Options
+              </label>
             </div>
           </div>
-           {/* <!--part Describe this journal input type text--> */}
-           <div className="col-12">
-             <h3>Describe this journal</h3>
-             <div className="input-group">
+          {/* <!--part Activity Duration input type number--> */}
+          <div className="col-12">
+            <h3>Activity Duration</h3>
+            <div className="input-group mb-3">
+              <input
+                type="number"
+                className="form-control"
+                placeholder="60"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <span className="input-group-text" id="basic-addon2">
+                mins
+              </span>
+            </div>
+          </div>
+          {/* <!--part Describe this journal input type text--> */}
+          <div className="col-12">
+            <h3>Describe this journal</h3>
+            <div className="input-group">
               <span className="input-group-text">textarea</span>
-              <textarea className="form-control" placeholder="Fun and Happy"aria-label="With textarea"></textarea>
+              <textarea
+                className="form-control"
+                placeholder="Fun and Happy"
+                aria-label="With textarea"
+              ></textarea>
             </div>
-            </div>
-          </div> 
           </div>
-          </section>
-    )
-}
+        </div>
+        <div class="col-12 mt-5">
+          <button
+            type="submit"
+            class="btn btn-secondary"
+            style={{ marginBottom: "30px" }}
+          >
+            Add activity
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default ActivityForm;
