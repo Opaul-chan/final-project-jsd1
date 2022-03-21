a323510e119aef61285e4fdcd23604043dfa1fe6
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-03-21 18:19:13 +0700

    "add activity form"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import ActivityIcon from "./Components/ActivityIcon/ActivityIcon.jsx";
 //import "./App.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
+import ActivityForm from "./Components/ActivityForm/ActivityForm.jsx";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ActivityIcon />
+      <ActivityForm />
     </div>
   );
 }
